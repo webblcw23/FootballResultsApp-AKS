5fa67142dd2cf6dec391572580662ae22a95136a
Lewis Webb <webblcw23@hotmail.com>
2025-11-07 04:45:04 +0000

    port updates

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -29,7 +29,7 @@ security_rule {
   access                     = "Allow"
   protocol                   = "Tcp"
   source_port_range          = "*"
-  destination_port_range     = "80"
+  destination_port_range     = "443"
   source_address_prefix      = "*"
   destination_address_prefix = "*"
 }
